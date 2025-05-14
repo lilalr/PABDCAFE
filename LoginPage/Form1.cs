3b4cfbf2cdc0357a1e74979398392a65f88d23da
lilalr <gurennut@gmail.com>
2025-05-14 19:14:01 +0700

    UBAH

--- LoginPage/Form1.cs
+++ LoginPage/Form1.cs
@@ -2,22 +2,21 @@
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 
 namespace PABDCAFE
 {
     public partial class Form1 : Form
     {
 
-        // ganti "server" sesuai dengan SQL server anda
-        private string connectionString = "Data Source=IDEAPAD5PRO\\LILA; Initial Catalog=ReservasiCafe; Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Dashboard_Load(object sender, EventArgs e)
         {
 
         }
