2229dbe999621c5bfaedb0efced7a1301d2a7e37
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-05-15 01:03:02 +0700

    jfuqivro1i2gr12r

--- LoginPage/Tambah.Designer.cs
+++ LoginPage/Tambah.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace LoginPage
+﻿namespace PABDCAFE
 {
-    partial class Tambah
+    partial class TambahPage
     {
         /// <summary>
         /// Required designer variable.
@@ -28,50 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtTrNama = new System.Windows.Forms.TextBox();
-            this.btnCustAdd = new System.Windows.Forms.Button();
-            this.txtTrNoTelp = new System.Windows.Forms.TextBox();
-            this.txtTrWaktu = new System.Windows.Forms.TextBox();
+            this.txtArNama = new System.Windows.Forms.TextBox();
+            this.btnArAdd = new System.Windows.Forms.Button();
+            this.txtArNoTelp = new System.Windows.Forms.TextBox();
+            this.txtArWaktu = new System.Windows.Forms.TextBox();
             this.ArNama = new System.Windows.Forms.Label();
             this.ArNoTelp = new System.Windows.Forms.Label();
             this.ArWaktu = new System.Windows.Forms.Label();
             this.dataGridViewTr = new System.Windows.Forms.DataGridView();
+            this.txtArMeja = new System.Windows.Forms.TextBox();
+            this.ArMeja = new System.Windows.Forms.Label();
+            this.btnArHapus = new System.Windows.Forms.Button();
+            this.btnArEdit = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTr)).BeginInit();
             this.SuspendLayout();
             // 
-            // txtTrNama
+            // txtArNama
             // 
-            this.txtTrNama.Location = new System.Drawing.Point(183, 22);
-            this.txtTrNama.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtTrNama.Name = "txtTrNama";
-            this.txtTrNama.Size = new System.Drawing.Size(273, 22);
-            this.txtTrNama.TabIndex = 0;
-            this.txtTrNama.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtArNama.Location = new System.Drawing.Point(183, 22);
+            this.txtArNama.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtArNama.Name = "txtArNama";
+            this.txtArNama.Size = new System.Drawing.Size(273, 22);
+            this.txtArNama.TabIndex = 0;
+            this.txtArNama.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // btnCustAdd
+            // btnArAdd
             // 
-            this.btnCustAdd.Location = new System.Drawing.Point(593, 42);
-            this.btnCustAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnCustAdd.Name = "btnCustAdd";
-            this.btnCustAdd.Size = new System.Drawing.Size(69, 29);
-            this.btnCustAdd.TabIndex = 1;
-            this.btnCustAdd.Text = "add";
-            this.btnCustAdd.UseVisualStyleBackColor = true;
+            this.btnArAdd.Location = new System.Drawing.Point(547, 28);
+            this.btnArAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnArAdd.Name = "btnArAdd";
+            this.btnArAdd.Size = new System.Drawing.Size(135, 29);
+            this.btnArAdd.TabIndex = 1;
+            this.btnArAdd.Text = "Tambah Reservasi";
+            this.btnArAdd.UseVisualStyleBackColor = true;
+            this.btnArAdd.Click += new System.EventHandler(this.btnArAdd_Click);
             // 
-            // txtTrNoTelp
+            // txtArNoTelp
             // 
-            this.txtTrNoTelp.Location = new System.Drawing.Point(183, 49);
-            this.txtTrNoTelp.Name = "txtTrNoTelp";
-            this.txtTrNoTelp.Size = new System.Drawing.Size(273, 22);
-            this.txtTrNoTelp.TabIndex = 2;
+            this.txtArNoTelp.Location = new System.Drawing.Point(183, 49);
+            this.txtArNoTelp.Name = "txtArNoTelp";
+            this.txtArNoTelp.Size = new System.Drawing.Size(273, 22);
+            this.txtArNoTelp.TabIndex = 2;
             // 
-            // txtTrWaktu
+            // txtArWaktu
             // 
-            this.txtTrWaktu.Location = new System.Drawing.Point(183, 77);
-            this.txtTrWaktu.Name = "txtTrWaktu";
-            this.txtTrWaktu.Size = new System.Drawing.Size(273, 22);
-            this.txtTrWaktu.TabIndex = 3;
-            this.txtTrWaktu.TextChanged += new System.EventHandler(this.txtWaktuReservasi_TextChanged);
+            this.txtArWaktu.Location = new System.Drawing.Point(183, 77);
+            this.txtArWaktu.Name = "txtArWaktu";
+            this.txtArWaktu.Size = new System.Drawing.Size(273, 22);
+            this.txtArWaktu.TabIndex = 3;
+            this.txtArWaktu.TextChanged += new System.EventHandler(this.txtWaktuReservasi_TextChanged);
             // 
             // ArNama
             // 
@@ -111,22 +116,61 @@
             this.dataGridViewTr.Size = new System.Drawing.Size(569, 150);
             this.dataGridViewTr.TabIndex = 7;
             // 
-            // Tambah
+            // txtArMeja
+            // 
+            this.txtArMeja.Location = new System.Drawing.Point(183, 106);
+            this.txtArMeja.Name = "txtArMeja";
+            this.txtArMeja.Size = new System.Drawing.Size(273, 22);
+            this.txtArMeja.TabIndex = 8;
+            // 
+            // ArMeja
+            // 
+            this.ArMeja.AutoSize = true;
+            this.ArMeja.Location = new System.Drawing.Point(65, 111);
+            this.ArMeja.Name = "ArMeja";
+            this.ArMeja.Size = new System.Drawing.Size(81, 16);
+            this.ArMeja.TabIndex = 9;
+            this.ArMeja.Text = "Nomor Meja";
+            // 
+            // btnArHapus
+            // 
+            this.btnArHapus.Location = new System.Drawing.Point(547, 62);
+            this.btnArHapus.Name = "btnArHapus";
+            this.btnArHapus.Size = new System.Drawing.Size(135, 29);
+            this.btnArHapus.TabIndex = 10;
+            this.btnArHapus.Text = "Hapus Reservasi";
+            this.btnArHapus.UseVisualStyleBackColor = true;
+            this.btnArHapus.Click += new System.EventHandler(this.btnArHapus_Click);
+            // 
+            // btnArEdit
+            // 
+            this.btnArEdit.Location = new System.Drawing.Point(547, 97);
+            this.btnArEdit.Name = "btnArEdit";
+            this.btnArEdit.Size = new System.Drawing.Size(135, 29);
+            this.btnArEdit.TabIndex = 11;
+            this.btnArEdit.Text = "Edit Reservasi";
+            this.btnArEdit.UseVisualStyleBackColor = true;
+            // 
+            // AdminReservasi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.btnArEdit);
+            this.Controls.Add(this.btnArHapus);
+            this.Controls.Add(this.ArMeja);
+            this.Controls.Add(this.txtArMeja);
             this.Controls.Add(this.dataGridViewTr);
             this.Controls.Add(this.ArWaktu);
             this.Controls.Add(this.ArNoTelp);
             this.Controls.Add(this.ArNama);
-            this.Controls.Add(this.txtTrWaktu);
-            this.Controls.Add(this.txtTrNoTelp);
-            this.Controls.Add(this.btnCustAdd);
-            this.Controls.Add(this.txtTrNama);
+            this.Controls.Add(this.txtArWaktu);
+            this.Controls.Add(this.txtArNoTelp);
+            this.Controls.Add(this.btnArAdd);
+            this.Controls.Add(this.txtArNama);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "Tambah";
-            this.Text = "TambahReservasi";
+            this.Name = "AdminReservasi";
+            this.Text = "AdminReservasi";
             this.Load += new System.EventHandler(this.Tambah_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTr)).EndInit();
             this.ResumeLayout(false);
@@ -135,13 +179,17 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button btnCustAdd;
-        private System.Windows.Forms.TextBox txtTrNoTelp;
-        private System.Windows.Forms.TextBox txtTrWaktu;
+        private System.Windows.Forms.Button btnArAdd;
+        private System.Windows.Forms.TextBox txtArNoTelp;
+        private System.Windows.Forms.TextBox txtArWaktu;
         private System.Windows.Forms.Label ArNama;
         private System.Windows.Forms.Label ArNoTelp;
         private System.Windows.Forms.Label ArWaktu;
-        private System.Windows.Forms.TextBox txtTrNama;
+        private System.Windows.Forms.TextBox txtArNama;
         private System.Windows.Forms.DataGridView dataGridViewTr;
+        private System.Windows.Forms.TextBox txtArMeja;
+        private System.Windows.Forms.Label ArMeja;
+        private System.Windows.Forms.Button btnArHapus;
+        private System.Windows.Forms.Button btnArEdit;
     }
 }
--- LoginPage/Tambah.cs
+++ LoginPage/Tambah.cs
@@ -1,40 +1,192 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
-namespace LoginPage
+namespace PABDCAFE
 {
-    public partial class Tambah : Form
+    public partial class TambahPage : Form
     {
-        public Tambah()
+        SqlConnection conn = new SqlConnection("Data Source=LAPTOP-4FJGLBGI\\NANDA; Initial Catalog=ReservasiCafe; Integrated Security=True;");
+        public TambahPage()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private bool ValidasiInput()
         {
+            // Validasi nama
+            if (string.IsNullOrWhiteSpace(txtArNama.Text))
+            {
+                MessageBox.Show("Nama tidak boleh kosong.");
+                return false;
+            }
 
+            // Validasi nomor telepon: hanya angka atau +62
+            string telp = txtArNoTelp.Text.Trim();
+            if (string.IsNullOrWhiteSpace(telp) ||
+                !(Regex.IsMatch(telp, @"^\+62\d{8,12}$") || Regex.IsMatch(telp, @"^\d{10,15}$")))
+            {
+                MessageBox.Show("Nomor telepon tidak valid. Gunakan format +62xxxxxxxxxx atau 08123456789.");
+                return false;
+            }
+
+            // Validasi waktu reservasi
+            if (!DateTime.TryParse(txtArWaktu.Text, out DateTime waktu))
+            {
+                MessageBox.Show("Format waktu tidak valid. Gunakan format yyyy-MM-dd HH:mm.");
+                return false;
+            }
+            if (waktu.Year != 2025)
+            {
+                MessageBox.Show("Reservasi hanya untuk tahun 2025.");
+                return false;
+            }
+
+            // Validasi nomor meja
+            if (string.IsNullOrWhiteSpace(txtArMeja.Text) || txtArMeja.Text.Length > 2)
+            {
+                MessageBox.Show("Nomor meja tidak valid (maks 2 digit).");
+                return false;
+            }
+
+            return true;
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void btnArAdd_Click(object sender, EventArgs e)
         {
+            if (!ValidasiInput()) return;
 
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(
+                    "INSERT INTO Reservasi (Nama_Customer, No_Telp, Waktu_Reservasi, Nomor_Meja) " +
+                    "VALUES (@Nama, @Telp, @Waktu, @Meja)", conn);
+                cmd.Parameters.AddWithValue("@Nama", txtArNama.Text.Trim());
+                cmd.Parameters.AddWithValue("@Telp", txtArNoTelp.Text.Trim());
+                cmd.Parameters.AddWithValue("@Waktu", DateTime.Parse(txtArWaktu.Text.Trim()));
+                cmd.Parameters.AddWithValue("@Meja", txtArMeja.Text.Trim());
+
+                int result = cmd.ExecuteNonQuery();
+                MessageBox.Show(result > 0 ? "Reservasi berhasil ditambahkan." : "Reservasi gagal ditambahkan.");
+
+                LoadReservasi();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
-        private void txtWaktuReservasi_TextChanged(object sender, EventArgs e)
+        private void CustomerPage_Load(object sender, EventArgs e)
         {
-
+            LoadReservasi();
         }
 
-        private void Tambah_Load(object sender, EventArgs e)
+        private void LoadReservasi()
         {
+            try
+            {
+                conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Reservasi", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridViewTr.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menampilkan data: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
 
+        private void btnCustHapus_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtArNama.Text) || string.IsNullOrWhiteSpace(txtArWaktu.Text))
+            {
+                MessageBox.Show("Masukkan nama dan waktu reservasi untuk menghapus.");
+                return;
+            }
+
+            if (!DateTime.TryParse(txtArWaktu.Text.Trim(), out DateTime waktu))
+            {
+                MessageBox.Show("Format waktu tidak valid.");
+                return;
+            }
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(
+                    "DELETE FROM Reservasi WHERE Nama_Customer = @Nama AND Waktu_Reservasi = @Waktu", conn);
+                cmd.Parameters.AddWithValue("@Nama", txtArNama.Text.Trim());
+                cmd.Parameters.AddWithValue("@Waktu", waktu);
+
+                int result = cmd.ExecuteNonQuery();
+                MessageBox.Show(result > 0 ? "Reservasi berhasil dihapus." : "Reservasi tidak ditemukan.");
+
+                LoadReservasi();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan saat menghapus: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtArNama.Text = "";
+            txtArNoTelp.Text = "";
+            txtArWaktu.Text = "";
+            txtArMeja.Text = "";
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Apakah Anda yakin ingin logout?", "Konfirmasi Logout", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                // Kembali ke LoginPage
+                LoginPage login = new LoginPage();
+                login.Show();
+                this.Close();
+            }
+            else
+            {
+                // cancel
+            }
+        }
+
+        private void btnCustLihat_Click(object sender, EventArgs e)
+        {
+            // panggil fungsi utama
+        }
+        private void dgvCutomer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // isi textbox ketika klik row di datagrid (jika perlu)
+            if (e.RowIndex >= 0 && dataGridViewTr.Rows.Count > e.RowIndex)
+            {
+                txtArNama.Text = dataGridViewTr.Rows[e.RowIndex].Cells["Nama_Customer"].Value?.ToString();
+                txtArNoTelp.Text = dataGridViewTr.Rows[e.RowIndex].Cells["No_Telp"].Value?.ToString();
+                txtArWaktu.Text = dataGridViewTr.Rows[e.RowIndex].Cells["Waktu_Reservasi"].Value?.ToString();
+                txtArMeja.Text = dataGridViewTr.Rows[e.RowIndex].Cells["Nomor_Meja"].Value?.ToString();
+            }
         }
     }
 }
