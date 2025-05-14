84dd8b333dfb91e3eba610d2095e8afac54f7800
MesiAnandaPutri <mesianandaputri24@gmail.com>
2025-05-14 19:22:48 +0700

    ubah

--- LoginPage/TambahReservasi.Designer.cs
+++ LoginPage/TambahReservasi.Designer.cs
@@ -30,31 +30,45 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnCustAdd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(135, 86);
+            this.textBox1.Location = new System.Drawing.Point(120, 69);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(307, 26);
+            this.textBox1.Size = new System.Drawing.Size(273, 22);
             this.textBox1.TabIndex = 0;
             // 
             // btnCustAdd
             // 
-            this.btnCustAdd.Location = new System.Drawing.Point(484, 86);
+            this.btnCustAdd.Location = new System.Drawing.Point(430, 69);
+            this.btnCustAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCustAdd.Name = "btnCustAdd";
-            this.btnCustAdd.Size = new System.Drawing.Size(78, 36);
+            this.btnCustAdd.Size = new System.Drawing.Size(69, 29);
             this.btnCustAdd.TabIndex = 1;
             this.btnCustAdd.Text = "add";
             this.btnCustAdd.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(439, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // TambahReservasi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnCustAdd);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "TambahReservasi";
             this.Text = "TambahReservasi";
             this.ResumeLayout(false);
@@ -66,5 +80,6 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnCustAdd;
+        private System.Windows.Forms.Button button1;
     }
 }
